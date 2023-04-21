6487437516113f4df6cfe12f38ce4f8e7180e6f4
xiangcitoZ <pzxiang1@gmail.com>
2023-04-21 11:19:34 +0200

    Add project files.

---
+++ ApiSaSTokenStorageTables/Controllers/TableTokenController.cs
@@ -0,0 +1,40 @@
+﻿using ApiSaSTokenStorageTables.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSaSTokenStorageTables.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TableTokenController : ControllerBase
+    {
+        private ServiceSaSToken service;
+
+        public TableTokenController(ServiceSaSToken service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        [Route("[action]/{curso}")]
+        public ActionResult<string> GenerateToken(string curso)
+        {
+            string token =
+                this.service.GenerateSaSToken(curso);
+            //PODEMOS PERSONALIZAR EL JSON QUE DEVOLVEMOS
+            // { numeroregistros: 5, datos: List<T> }
+            //return Ok(
+            //    new
+            //    {
+            //        numeroregistros= 5,
+            //        datos = new List<int>()
+            //    });
+            //{ token: TOKENVALUE }
+            return Ok(new
+            {
+                token = token
+            });
+        }
+
+    }
+}
---
+++ ApiSaSTokenStorageTables/Services/ServiceSaSToken.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Data.Tables.Sas;
+using Azure.Data.Tables;
+
+namespace ApiSaSTokenStorageTables.Services
+{
+    public class ServiceSaSToken
+    {
+        //ESTA CLASE GENERARA TOKENS A PARTIR DE LA 
+        //TABLA ALUMNOS
+        private TableClient tablaAlumnos;
+
+        public ServiceSaSToken(IConfiguration configuration)
+        {
+            string azureKeys =
+                configuration.GetValue<string>("AzureKeys:StorageAccount");
+            TableServiceClient serviceClient =
+                new TableServiceClient(azureKeys);
+            this.tablaAlumnos = serviceClient.GetTableClient("alumnos");
+        }
+
+        //QUEREMOS UN TOKEN QUE SOLAMENTE NOS DEVUELVA LOS ALUMNOS
+        //DE UN DETERMINADO CURSO.
+        public string GenerateSaSToken(string curso)
+        {
+            //NECESITAMOS LOS PERMISOS DE ACCESO
+            TableSasPermissions permisos =
+                 TableSasPermissions.Read;
+            //DEBEMOS CREAR UN CONSTRUCTOR CON LOS PERMISOS 
+            //Y EL TIEMPO DE ACCESO A LA TABLA
+            TableSasBuilder builder =
+                this.tablaAlumnos.GetSasBuilder(permisos,
+                DateTime.Now.AddMinutes(50));
+            //QUEREMOS QUE SOLAMENTE NOS MUESTRE LOS CURSOS
+            //(PARTITION KEY)
+            //JAVA
+            builder.PartitionKeyStart = curso;
+            builder.PartitionKeyEnd = curso;
+            //CON TODO ESTO MONTADO, NOS DARA UNA URI
+            //DE ACCESO
+            Uri uriToken =
+                this.tablaAlumnos.GenerateSasUri(builder);
+            //EXTRAEMOS LA RUTA HTTPS CON EL TOKEN
+            string token = uriToken.AbsoluteUri;
+            return token;
+        }
+
+    }
+}
